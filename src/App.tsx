00ce3a487e9c309e2cb8fbf8fcdf8093895e5a5e
frhs3011 <frhs3011@dsb.dk>
2024-08-30 12:27:35 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  //const meow: string = "hey";
+
+  //const tst = 1;
+  //console.log(tst, meow);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button
+          onClick={() => {
+            setCount((count: number) => count + 1);
+          }}
+        >
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
